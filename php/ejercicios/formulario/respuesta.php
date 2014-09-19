5b6d597ab454bcb163c665a4fa9c135b7a0ccaa5
eliAgirre <eli91ai@gmail.com>
2014-09-19 17:12:38 +0200

    añadir formulario

---
+++ php/ejercicios/formulario/respuesta.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+    <?php
+	echo "Nombre: " . $_GET["nombre"] . '</br>'; 
+	echo "Apellido: " .$_GET["apellido"] . '</br>'; 
+	echo "Edad: " . $_GET["edad"] . '</br>';  
+	echo "Email: " . $_GET["email"] . '</br>'; 
+	?>
+</body>
+</html>
